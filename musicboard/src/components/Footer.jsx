07123ed5ec9683c8d179408e7d3f631b4ad89387
Thielicious <michel.thiel@gmx.net>
2022-07-19 14:00:47 +0200

    auth

--- musicboard/src/components/Footer.jsx
+++ musicboard/src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => (
 			</div>
 		</div>
 		<div id="social-links">
-			<span class="social-links__wrapper">
+			<span className="social-links__wrapper">
 				<Link to="/">
 					<svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
 						<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
--- musicboard/src/components/Login.jsx
+++ musicboard/src/components/Login.jsx
@@ -1,40 +1,70 @@
-import { useRef, useState, useEffect } from react
+import { useRef, useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 
 const Login = () => {
 
-	const userRef = useRef()
-	const errRef = useRef()
+	const
+		userRef = useRef(),
+		errRef = useRef(),
 
-	const abc = 'def'
+		[user, setUser] = useState(''),
+		[pwd, setPwd] = useState(''),
+		[errMsg, setErrMsg] = useState(''),
+		[success, setSuccess] = useState(false)
 
-	let def = 12
+	useEffect(() => {
+		userRef.current.focus()
+	}, [])
 
-	errRef().test()
+	useEffect(() => {
+		setErrMsg('')
+	}, [user, pwd])
 
-	abc.blah = null || false || true
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		console.log(user, pwd)
+		setUser('')
+		setPwd('')
+		setSuccess(true)
+	}
 
 	return (
-		<>
-			<section className="section -mt-5">
-				<div className="mb-10">
-					<h1 className="section__headline">LOG IN</h1>
-				</div>
-				<div className="login">
-					<form className="loginform">
-						<label htmlFor="username">Username</label>
-						<input className="border-4" type="text" id="username" onChange="" value="" required />
-						<label htmlFor="password">Password</label>
-						<input className="border-4" type="password" id="password" onChange="" value="" required />
-						<input id="login" type="button" value="Log In" />
-					</form><br />
-					<p>
-						<Link className="link" to="/forgotpw">➤ Forgot Password?</Link><br />
-						<Link className="link" to="/register">➤ Register</Link>
-					</p>
-				</div>
-			</section>
-		</>
+		<section className="section -mt-5">
+			{success ? (
+				<>
+					<div className="mb-10">
+						<h1 className="section__headline">Customer Panel</h1>
+					</div>
+					<div className="login">
+						<p>You are logged in!</p>
+					</div>
+				</>
+			) : (
+				<>
+					<div className="mb-10">
+						<h1 className="section__headline">LOG IN</h1>
+					</div>
+					<div className="login">
+						<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+						<form className="login__form" onSubmit={handleSubmit}>
+							<label htmlFor="username">Username</label>
+							<input className="border-4" type="text" id="username"
+								ref={userRef} onChange={(e) => setUser(e.target.value)} value={user} required
+							/>
+							<label htmlFor="password">Password</label>
+							<input className="border-4" type="password" id="password"
+								onChange={(e) => setPwd(e.target.value)} value={pwd} required
+							/>
+							<input id="login" type="submit" value="Log In" />
+						</form><br />
+						<p>
+							<Link className="link" to="/forgotpw">➤ Forgot Password?</Link><br />
+							<Link className="link" to="/register">➤ Register</Link>
+						</p>
+					</div>
+				</>
+			)}
+		</section>
 	)
 }
 
