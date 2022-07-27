75bd9e9d78dcafb3ed6408ab573233b542847dde
Thielicious <michel.thiel@gmx.net>
2022-07-27 08:26:20 +0200

    1

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/news', function () {
-    return json_encode(['News' => '1']);
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
